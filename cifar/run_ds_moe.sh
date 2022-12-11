82532b891975b84dc90aa18fdb891ef8a9c32584
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2022-12-11 17:09:56 -0500

    Update run_ds_moe.sh

--- cifar/run_ds_moe.sh
+++ cifar/run_ds_moe.sh
@@ -3,13 +3,13 @@
 # Number of nodes
 NUM_NODES=1
 # Number of GPUs per node
-NUM_GPUS=2
+NUM_GPUS=4
 # Size of expert parallel world (should be less than total world size)
 EP_SIZE=2
 # Number of total experts
-EXPERTS=2
+EXPERTS=8
 
-deepspeed --num_nodes=${NUM_NODES} --num_gpus=${NUM_GPUS} cifar10_deepspeed.py \
+/home/scttmars/.local/bin/deepspeed --num_nodes=${NUM_NODES} --num_gpus=${NUM_GPUS} cifar10_deepspeed.py \
 	--log-interval 100 \
 	--deepspeed \
 	--deepspeed_config ds_config.json \
